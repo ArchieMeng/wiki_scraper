1f0164bc32d89e7ab1e04858d8df7da70d90c852
ArchieMeng <506759765@qq.com>
2017-01-01 18:19:37 +0800

    use deque as stack in main.py

--- main.py
+++ main.py
@@ -4,54 +4,70 @@ import re
 from graph import Graph
 import pickle
 from sendmail import send_mail
+from collections import deque
 
 
-def getlinks(sublink, deepth=1):
+def getlinks(sublink, depth=1):
     try:
         graph_file = open('wiki' + sublink[5:] + '.pickle', 'r')
-        page_list = []
+        page_list = deque()
         graph = Graph(pickle.load(graph_file).get_neighbour())
         for v in graph.vertices():
             if graph.get_degree(v)['out'] == 0:
-                page_list.append((v, deepth))
-        send_mail('506759765@qq.com', 'Task: scrapying ' + sublink + ' continuing', 'with deepth:' + str(deepth))
+                page_list.append((v, depth))
+        try:
+            send_mail('506759765@qq.com',
+                      'Task: scrapying ' + sublink + ' continuing',
+                      'with deepth:' + str(depth))
+        except:
+            print "email is not sent"
+
     except:
         # None exist
-        page_list = [(sublink, deepth)]
+        page_list = deque([(sublink, depth)])
         graph = Graph()
-        send_mail('506759765@qq.com', 'Task: scrapying ' + sublink + ' started', 'deepth:' + str(deepth))
+        try:
+            send_mail('506759765@qq.com',
+                      'Task: scrapying ' + sublink + ' started',
+                      'deepth:' + str(depth))
+        except:
+            print "email is not sent"
 
     # while page_list is not empty
     while page_list:
-        page = tuple(page_list.pop())
-        (page,page_deepth) = page
+        page = tuple(page_list.popleft())
+        (page, page_depth) = page
 
         # if reach max deepth, ignore it
-        if page_deepth is 0:
-            continue
+        if page_depth > 0:
+            req = urlopen(wikiurl+page)
+            content = req.read()
+            parent_page = page
 
-        req = urlopen(wikiurl+page)
-        content = req.read()
-        parent_page = page
+            # use lxml first
+            try:
+                soup = BeautifulSoup(content,'lxml')
+            except:
+                soup = BeautifulSoup(content)
 
-        # use lxml first
-        try:
-            soup = BeautifulSoup(content,'lxml')
-        except:
-            soup = BeautifulSoup(content)
+            links = soup.findAll("a",{'href':re.compile('^(/wiki/)[^:]*?$')})
+            for link in links:
+                page = link.attrs['href']
+                if page not in graph.vertices():
+                    print '->'+page
+                    page_list.appendleft((page,page_depth - 1))
+                graph.add_edge([parent_page, page])
 
-        links = soup.findAll("a",{'href':re.compile('^(/wiki/)[^:]*?$')})
-        for link in links:
-            page = link.attrs['href']
-            if page not in graph.vertices():
-                print '->'+page
-                page_list.append((page,page_deepth-1))
-            graph.add_edge([parent_page, page])
     graph_file = open('wiki'+sublink[5:]+'.pickle','wb')
     pickle.dump(graph,graph_file,2)
     graph_file.close()
     v_num = len(graph.vertices())
-    send_mail('506759765@qq.com','The scrapy for '+sublink+' is done!',str(v_num)+' vertices was reached!\n')
+    try:
+        send_mail('506759765@qq.com',
+                  'The scrapy for '+sublink+' is done!',
+                  str(v_num)+' vertices was reached!\n')
+    except:
+        print 'email is not sent'
 
 wikiurl = "http://en.wikipedia.org"
 print wikiurl
